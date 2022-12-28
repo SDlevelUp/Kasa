f963df370ff858f3d4b844bc9618b5a5fa2c3dcf
Sarah Berri <sarahberri@outlook.fr>
2022-12-28 21:11:17 +0100

    Add Carrousel files

---
+++ kasa/src/components/Carrousel/Carrousel.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import leftArrow from "../../assets/leftArrow.svg";
+import rightArrow from "../../assets/rightArrow.svg";
+
+import "./Carrousel.css";
+
+function Carrousel({ pictures }) {
+    const [current, setCurrent] = useState(0);
+
+    const length = pictures.length;
+
+    const rightSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+
+    const leftSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    };
+
+    return (
+        <>
+            {pictures.map((picture, index) =>
+                current === index && (
+                    <div key={picture} className="carrousel_pictures">
+                        <span className="carrousel_count_display">
+                            {index + 1}/{length}
+                        </span>
+                        <img src={picture} alt="Rental" />
+                        <div className="carrousel_leftArrow" onClick={leftSlide}>
+                            <img src={leftArrow} alt="Preview arrow" />
+                        </div>
+                        <div className="carrousel_rightArrow" onClick={rightSlide}>
+                            <img src={rightArrow} alt="Next arrow" />
+                        </div>
+                    </div>
+                )
+            )}
+        </>
+    );
+}
+
+export default Carrousel;
