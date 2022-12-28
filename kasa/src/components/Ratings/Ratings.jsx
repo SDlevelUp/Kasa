3294fbd12d5bd4114024e4601734c9115a3ab806
Sarah Berri <sarahberri@outlook.fr>
2022-12-29 00:50:59 +0100

    changes code rating jsx and css

--- kasa/src/components/Ratings/Ratings.jsx
+++ kasa/src/components/Ratings/Ratings.jsx
@@ -1,6 +1,26 @@
-export const Ratings = ({ selected = false }) => (
-    <span className={`${selected ? "red" : "grey"}`}>{"★"}</span>
-);
+import React from "react";
+import "./Ratings.css"
 
+import redStar from "../../assets/redStar.png"
+import greyStar from "../../assets/greyStar.png"
+
+
+function Ratings(props) {
+    const ratingScale = props.ratingScale
+    const order = [1, 2, 3, 4, 5]
+
+    return (
+
+        <div className="rental_ratings">
+            {order.map((orderElem) =>
+                ratingScale >= orderElem ?
+                    <img key={orderElem.toString()} src={redStar} alt="Rating Scale" /> :
+                    <img key={orderElem.toString()} src={greyStar} alt="Rating Scale" />
+            )}
+        </div>
+    )
+}
+
+export default Ratings;
 
 
