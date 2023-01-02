5dd2764e5a4120845b159ea26e1893364d74905c
Sarah Berri <sarahberri@outlook.fr>
2023-01-02 22:14:21 +0100

    modif card display and about

--- kasa/src/pages/About/About.jsx
+++ kasa/src/pages/About/About.jsx
@@ -1,17 +1,16 @@
 import "./About.css";
 import Collapse from "../../components/Collapse/Collapse";
-import banner from "../../assets/bannerAbout.png";
-import Banner from "../../components/Banner/Banner";
-import bannerMobile from "../../assets/bannerAboutMobile.png"
+// import banner from "../../assets/bannerAbout.png";
+// import Banner from "../../components/Banner/Banner";
 import BannerAbout from "../../components/BannerAbout/BannerAbout";
 
 
 function About({ data }) {
     return (
         <div className="about_page">
-            <Banner className="about_banner" image={banner} />
+            {/* <Banner className="about_banner" image={banner} /> */}
             <section>
-                <BannerAbout image={bannerMobile} />
+                <BannerAbout />
             </section>
             <div className="collapse_about">
                 {data.map((item) => (
